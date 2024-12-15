911d2ca2d3214038533cf9888e59ec6af8967945
BrawlBreed <99895434+BrawlBreed@users.noreply.github.com>
2024-12-15 06:58:22 +0200

    Final

--- src/features/constants.js
+++ src/features/constants.js
@@ -1,4 +1,4 @@
 export const API_URL =
     process.env.MODE === 'development'
     ? 'http://localhost:8080'
-    : `https://p-pay-gilt.vercel.app` || import.meta.env.VITE_VERCEL_URL;
+    : `https://p-pay-gilt.vercel.app`;
