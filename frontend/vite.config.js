e3cdfbf003343308bd2246359fbe3b6cd732035a
BrawlBreed <99895434+BrawlBreed@users.noreply.github.com>
2024-12-15 04:29:38 +0200

    Hopeless

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.svg'],
+  },
   build: {
-    outDir: '../dist',
-    assetsDir: 'assets'
+    outDir: '../dist'
   },
   base: '/',
   server: {
