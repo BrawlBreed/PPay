29bccbb035785052705c8154c9e79597755bda6d
BrawlBreed <99895434+BrawlBreed@users.noreply.github.com>
2024-12-15 06:44:53 +0200

    Final

--- src/features/constants.js
+++ src/features/constants.js
@@ -1,4 +1,4 @@
 export const API_URL =
-    process.env.NODE_ENV === 'development'
+    process.env.MODE === 'development'
     ? 'http://localhost:8080'
     : `https://${process.env.VITE_VERCEL_URL}`
